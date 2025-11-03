8853017b3aa7a4bde25ff029f741f8d574de9fdc
naveen sanjula <82176749+naveensanjula975@users.noreply.github.com>
2025-11-03 23:00:29 +0530

    feat: initialize frontend with TypeScript, Vite, and Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-yellow-500">
+      <h1 className="text-center text-8xl">hello world</h1>
+    </div>
+  )
+}
+
+export default App
